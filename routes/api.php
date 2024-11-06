78e2d7f50e850455080919095bcdf5220f2ef39a
zobirofkir <zobirofkir19@gmail.com>
2024-11-06 14:03:38 +0100

    make api.php clean

--- routes/api.php
+++ routes/api.php
@@ -10,27 +10,53 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
+
+/**
+ * Add Login Route
+ */
 Route::post("login", [AuthController::class, "login"]);
 
+/**
+ * Add Blog Route
+ */
 Route::apiResource("blogs", BlogController::class)->only(["index", "show"]);
+
+/**
+ * Add Comment Routes
+ */
 Route::apiResource("blogs.comments", CommentController::class)->only(["store", "index"]);
 
+/**
+ * Add Contact Routes
+ */
 Route::apiResource('contacts', ContactController::class);
+
+/**
+ * Add Product Routes
+ */
 Route::apiResource("products", ProductController::class)->only(["index", "show"]);
 
+/**
+ * Add Project Routes
+ */
 Route::apiResource("projects", ProjectController::class)->only(["index", "show"]);
 
 Route::apiResource("messages", MessageController::class);
 
-// Authenticated routes
+/**
+ * Add User Routes
+ */
 Route::middleware("auth:api")->group(function() {
     Route::apiResource("users", UserController::class);
-    
-    // Only add the store, update, and destroy methods for blogs here
+
+    /**
+     * Create Blog Resource
+     */
     Route::apiResource("blogs", BlogController::class)->only(["store", "update", "destroy"]);
 
-    // Add route for authenticated user's blogs
+    /**
+     * Add route for authenticated user's blogs
+     */
     Route::get("auth/blogs", [BlogController::class, "auth"]);
 
     /**
@@ -38,7 +64,14 @@ Route::middleware("auth:api")->group(function() {
      */
     Route::apiResource("products", ProductController::class)->only(["store", "update", "destroy"]);
 
+    /**
+     * Create Project Resource
+     */
     Route::apiResource("projects", ProjectController::class)->only(["store", "update", "destroy"]);
+    
+    /**
+     * Get Authenticated Project
+     */
     Route::get("/auth/projects", [ProjectController::class, "auth"]);
 
     /**
@@ -46,8 +79,18 @@ Route::middleware("auth:api")->group(function() {
      */
     Route::get("/auth/products", [ProductController::class, "auth"]);
     
-    // Authenticated user routes
+    /**
+     * Get Current Authenticated User
+     */
     Route::get("auth/current", [AuthController::class, "current"]);
+
+    /**
+     * Update Current User Password
+     */
     Route::put("auth/update", [AuthController::class, "updateCurrentUserPassword"]);
+    
+    /**
+     * Logout Authenticated User
+     */
     Route::post("auth/logout", [AuthController::class, "logout"]);  // Changed from GET to POST
 });
